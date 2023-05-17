76d31042739bf75627814850b0819e29feff0330
aosman <pianogreen0@gmail.com>
2023-05-17 11:05:12 +0100

    using css modules

--- port/src/components/Navabr.jsx
+++ port/src/components/Navabr.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import classes from './Navbar.module.css'
 
 export default function Navabr() {
   const Section = styled.div`
@@ -9,13 +10,37 @@ export default function Navabr() {
 const Container = styled.div`
 width: 1500px;
 background-color:rebeccapurple;
-text-align:center;
+
 `
+
   return (
-    <Section>
-      <Container>
-      Navbar
-        </Container>
-      </Section>
+
+    <div className={classes.container}>
+      <div className={classes.nav}>
+       
+      </div>
+
+    </div>
+
+
+    // <Section>
+    //   <Container>
+    //     <Links>
+    //     <List>
+    //       <ListItem>Home</ListItem>
+    //       <ListItem>About</ListItem>
+    //       <ListItem>Projects</ListItem>
+    //       <ListItem>Contact</ListItem>
+    //     </List>
+    //     </Links>
+    //     <Icons>
+    //     <img width="48" height="48" src="https://img.icons8.com/color/48/search--v1.png" alt="search--v1"/>
+    //     <Button>Hire Now</Button>
+    //     </Icons>
+        
+
+    //   Navbar
+    //     </Container>
+    //   </Section>
   )
 }
