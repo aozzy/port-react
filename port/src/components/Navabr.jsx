f4ce9980839ed8915028010e08d9cc95164c4a57
aosman <pianogreen0@gmail.com>
2023-05-17 13:39:49 +0100

    using css modules

--- port/src/components/Navabr.jsx
+++ port/src/components/Navabr.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+
 import classes from './Navbar.module.css'
 
 export default function Navabr() {
-  const Section = styled.div`
-  display:flex;
-  justify-content: center;
-`
-const Container = styled.div`
-width: 1500px;
-background-color:rebeccapurple;
-
-`
+  
 
   return (
 
     <div className={classes.container}>
       <div className={classes.nav}>
+       <div className={classes.links}>
+        logo goes here
+       <ul className={classes.navbar}>
+        <li className={classes.navItem}>Home</li>
+        <li className={classes.navItem}>About</li>
+        <li className={classes.navItem}>Projects</li>
+        <li className={classes.navItem}>Contact</li>
+       </ul>
+       </div>
+      <div className={classes.icon}>
+       <img width="48" height="48" src="https://img.icons8.com/color/48/search--v1.png" alt="search--v1"/>
+       <button className={classes.btn}>Hire Me </button>
        
       </div>
-
+      </div>
     </div>
 
 
