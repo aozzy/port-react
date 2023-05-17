cfa9975cbdc604f8eb133d62191a1f2b059f0a18
aosman <pianogreen0@gmail.com>
2023-05-17 14:26:42 +0100

    using css modules

--- port/src/components/Main.jsx
+++ port/src/components/Main.jsx
@@ -6,7 +6,15 @@ export default function Main() {
   return (
     <div className={classes.hero}>
       <Navbar/>
-      Main
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <h1>Hi, my name is</h1>
+          <h2>Abdi Osman</h2>
+          <h3>I build things for the web</h3>
+          <p>I'm a Software Engineer that welcomes challenges and architects solutions.</p>
+        </div>
+        <div className={classes.right}></div>
+      </div>
 
     </div>
   )
