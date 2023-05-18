f707c68a11ce48e96dafe806797d7134f8bc6d98
aosman <pianogreen0@gmail.com>
2023-05-18 16:51:45 +0100

    using css modules

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -3,36 +3,22 @@ import Main from './components/Main'
 import  Experience  from './components/Experience'
 import About  from './components/About'
 import Contact from './components/Contact'
+import './index.css'
 
-const Container = styled.div`
-height: 100vh;
-scroll-snap-type: y mandatory;
-scroll-behavior: smooth;
-overflow-y: auto;
-scrollbar-width: none;
-  &::-webkit-scrollbar{
-    display: none;
-  }
-  background: #a8c0ff;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #3f2b96, #a8c0ff);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #3f2b96, #a8c0ff); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-
-`
 
 function App() {
  
 
   return (
    
-      <Container>
+      <div className='container'>
      
 
       <Main/>
-      <Experience/>
       <About/>
+      <Experience/>
       <Contact/>
-      </Container>
+      </div>
     
   )
 }
--- port/src/components/About.jsx
+++ port/src/components/About.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import classes from './About.module.css'
 
 export default function About () {
-  const Section = styled.div`
-height: 100vh;
-
-scroll-snap-align: center;
-`
+  
   return (
-    <Section>About</Section>
+    <div className={classes.section}>
+    
+    <div className={classes.container}>
+      <div className={classes.left}>
+      </div>
+      <div className={classes.right}>
+        <h1 className={classes.title}>Hi, my name is</h1>
+        <h2 className={classes.subtitle}>Abdi Osman</h2>
+        <h2 className={classes.subtitle}>I build things for the web</h2>
+        <p className={classes.description}>I'm a Software Engineer that welcomes challenges and architects solutions.</p>
+        <button className={classes.btn}>Learn More</button>
+       
+      </div>
+    </div>
+
+  </div>
   )
 }
