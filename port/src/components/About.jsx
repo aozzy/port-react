96f65804b5c584746ad282fc8fe98e547b2f53fe
aosman <pianogreen0@gmail.com>
2023-05-19 11:33:46 +0100

    About section

--- port/src/components/About.jsx
+++ port/src/components/About.jsx
@@ -10,10 +10,12 @@ export default function About () {
       <div className={classes.left}>
       </div>
       <div className={classes.right}>
-        <h1 className={classes.title}>Hi, my name is</h1>
-        <h2 className={classes.subtitle}>Abdi Osman</h2>
-        <h2 className={classes.subtitle}>I build things for the web</h2>
-        <p className={classes.description}>I'm a Software Engineer that welcomes challenges and architects solutions.</p>
+        <h1 className={classes.title}>About Me</h1>
+        {/* <h2 className={classes.subtitle}>Abdi Osman</h2>
+        <h2 className={classes.subtitle}>I build things for the web</h2> */}
+        <p className={classes.description}>
+        I am a keen tech enthusiast with experience in cloud computing, IT and infrastructure support and I’m now seeking to develop my career in the Software development industry. To enhance my skills in Software development, I have completed the Software Engineering Immersive course at General Assembly. I continually try to develop my skill set by keeping abreast of new Technology. I’m currently looking for a Junior developer role as a front end or full stack engineer. Please see my Linkedin profile for my previous roles or get in touch to request a full CV.
+        </p>
         <button className={classes.btn}>Learn More</button>
        
       </div>
