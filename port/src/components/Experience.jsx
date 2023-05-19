e7987ff63a376c55067d850109696c9b2821daa7
aosman <pianogreen0@gmail.com>
2023-05-19 12:04:55 +0100

    Experience section

--- port/src/components/Experience.jsx
+++ port/src/components/Experience.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import classes from './Experience.module.css'
 export default function Experience() {
   
+  const data = [
+    "Full stack development",
+    "Infrastructure & IT support",
+    "Cloud computing"
+  ]
 
   return (
-  <div className={classes.section}>Experience</div>
+  <div className={classes.section}>Experience
+  <div className={classes.container}>
+    <div className={classes.left}>
+     <ul>
+      {data.map(item => <li>{item}</li>)}
+     </ul>
+    </div>
+    <div className={classes.right}>
 
+    </div>
+  </div>
+
+  </div>
+  
   )
 }
