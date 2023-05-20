68fc7ed3cb9328739bff3bdb88d2e4555ac566fe
aosman <pianogreen0@gmail.com>
2023-05-20 11:20:26 +0100

    contact section

--- port/src/components/Contact.jsx
+++ port/src/components/Contact.jsx
@@ -4,7 +4,21 @@ import classes from './Contact.module.css'
 function Contact() {
   
   return (
-    <div className={classes.section}>Contact</div>
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <form className={classes.form} action="">
+            <h2 className={classes.title}>Contact</h2>
+            <input className={classes.input} placeholder='Name' type="text" />
+            <input className={classes.input} placeholder='Email' type="email" />
+             <textarea className={classes.textarea} name="" id="" cols="30" rows="10"></textarea>
+             <button className={classes.btn}>Send</button>
+          </form>
+        </div>
+        <div className={classes.right}></div>
+
+      </div>
+    </div>
   )
 }
 
