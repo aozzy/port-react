e6b17abe113347538c1432edd49ab5b44a814dbc
aosman <pianogreen0@gmail.com>
2023-05-22 12:29:47 +0100

    email for contact section

---
+++ port/src/components/three.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './three.modules.css'
+
+export default function three() {
+  return (
+    <div className={classes.container}>
+      <canvas></canvas>
+    </div>
+  )
+}
