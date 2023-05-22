754a9e17eb5ac519d0234c338130204ecca0404d
aosman <pianogreen0@gmail.com>
2023-05-22 15:19:07 +0100

    3d models

--- port/src/components/ThreeModel.jsx
+++ port/src/components/ThreeModel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import classes from './ThreeModel.module.css'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera, RenderTexture,Text } from '@react-three/drei'
 
 
 export default function ThreeModel() {
@@ -13,10 +13,16 @@ export default function ThreeModel() {
   
           <OrbitControls enableZoom={false}/>
           <ambientLight intensity={2}/>
-          <directionalLight position={[5,5,5]}/>
+          <directionalLight position={[1,1,1]}/>
         <mesh>
-          <boxGeometry args={[1,1,1]}/>
-          <meshStandardMaterial color="green"/>
+          <boxGeometry args={[2,2,2]}/>
+          <meshStandardMaterial>
+            <RenderTexture attach='map'>
+              <PerspectiveCamera makeDefault position={[0,0,2]}/>
+              <color attach="background" args={['yellow']}/>
+              <Text fontSize={0.5} color="#555">`hello world`</Text>
+            </RenderTexture>
+          </meshStandardMaterial>
          
           </mesh>  
       </Canvas>
