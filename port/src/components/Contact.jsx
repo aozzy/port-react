126515fb26c02259db9f78ed2b4746d283c0542f
aosman <pianogreen0@gmail.com>
2023-05-22 12:23:55 +0100

    email for contact section

--- port/src/components/Contact.jsx
+++ port/src/components/Contact.jsx
@@ -1,17 +1,20 @@
-import React, { useRef }from 'react'
+import React, { useRef,useState }from 'react'
 import emailjs from '@emailjs/browser'
 import classes from './Contact.module.css'
 import Map from './Map'
 
 function Contact() {
   const form = useRef();
+  const [isError,setIsError] = useState(null)
 const handleSubmit = (e) => {
   e.preventDefault()
-  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+  emailjs.sendForm('service_nyoyae2', 'template_rby5hxq', form.current, 'RCSBYoAu5e8OgHNhN')
   .then((result) => {
       console.log(result.text);
-  }, (error) => {
+      setIsError(true)
+    }, (error) => {
       console.log(error.text);
+      setIsError(false)
   });
 }
 
@@ -19,12 +22,13 @@ const handleSubmit = (e) => {
     <div className={classes.section}>
       <div className={classes.container}>
         <div className={classes.left}>
-          <form className={classes.form} action="" onSubmit={handleSubmit}>
+          <form className={classes.form} action="" onSubmit={handleSubmit} ref={form}>
             <h2 className={classes.title}>Contact</h2>
-            <input className={classes.input} placeholder='Name' type="text" />
-            <input className={classes.input} placeholder='Email' type="email" />
-             <textarea className={classes.textarea} placeholder='Type message here' name="" id="" cols="30" rows="10"></textarea>
+            <input className={classes.input} placeholder='Name' type="text" name='name' />
+            <input className={classes.input} placeholder='Email' type="email" name='email'/>
+             <textarea className={classes.textarea} placeholder='Type message here' name="message"  cols="30" rows="10"></textarea>
              <button className={classes.btn}>Send</button>
+             {isError && <div className={classes.sent}>Your message has been sent sucessfully</div>}
           </form>
         </div>
         <div className={classes.right}>
