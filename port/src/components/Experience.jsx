420c123433b4e69a4b28456c570cf62f2ee46d71
aosman <pianogreen0@gmail.com>
2023-05-23 11:47:24 +0100

    3d models

--- port/src/components/Experience.jsx
+++ port/src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import classes from './Experience.module.css'
 export default function Experience() {
   
@@ -8,7 +8,7 @@ export default function Experience() {
     "Cloud computing",
     "Cyber security"
   ]
-
+  const [listItem,setListItem] = useState('projects')
   return (
   <div className={classes.section}>
   <div className={classes.container}>
---
+++ port/src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Projects.module.css'
+
+
+export default function Projects() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
