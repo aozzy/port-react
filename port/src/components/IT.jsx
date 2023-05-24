060569aaa94c085081e7e8fc5af4520a378c2f70
aosman <pianogreen0@gmail.com>
2023-05-24 11:51:41 +0100

    updated desktop model

--- port/src/components/IT.jsx
+++ port/src/components/IT.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
+import { PerspectiveCamera, RenderTexture,Text } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import Desktop from './Desktop'
 
 export default function IT() {
   return (
     <Canvas>
       <Stage environment={'city'} intensity={1}>
-
+      <PerspectiveCamera makeDefault position={[0,0,0]}/>
       <Desktop/>
       </Stage>
-      {/* <Memcard/> */}
+  
       <OrbitControls enableZoom={false}/>
     </Canvas>
   )
