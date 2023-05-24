47f0ca4517a6797a3ef03c0608612600bdadea1e
aosman <pianogreen0@gmail.com>
2023-05-24 13:38:43 +0100

    updated desktop model

--- port/src/components/IT.jsx
+++ port/src/components/IT.jsx
@@ -4,16 +4,24 @@ import { OrbitControls, Stage } from '@react-three/drei'
 import { PerspectiveCamera, RenderTexture,Text } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import Desktop from './Desktop'
+import classes from './IT.module.css'
 
 export default function IT() {
   return (
-    <Canvas>
+    <React.Fragment>
+
+    <Canvas camera={{fov:35,position:[5,5,5]}}>
       <Stage environment={'city'} intensity={1}>
-      <PerspectiveCamera makeDefault position={[0,0,0]}/>
-      <Desktop/>
+      <PerspectiveCamera makeDefault position={[1.5,0.5,1]}/>
+      <mesh  scale={[1,1,1]}>
+
+      <Desktop />
+      </mesh>
       </Stage>
   
       <OrbitControls enableZoom={false}/>
     </Canvas>
+    <p className={classes.description}>I created this model using Blender</p>
+    </React.Fragment>
   )
 }
