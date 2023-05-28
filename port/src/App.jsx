2851e34c62c2697d445dfd3c4ff808fa68b1b800
aosman <pianogreen0@gmail.com>
2023-05-28 11:29:45 +0100

    adding lottie animation

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -1,30 +1,20 @@
-import styled from 'styled-components'
-import Main from './components/Main'
-import  Experience  from './components/Experience'
-import About  from './components/About'
-import Contact from './components/Contact'
+import styled from "styled-components";
+import Main from "./components/Main";
+import Experience from "./components/Experience";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-
-import './index.css'
-
+import "./index.css";
 
 function App() {
- 
-
   return (
-   
-      <div className='container'>
-     
-
-      <Main/>
-       <About/>
-       <Experience/>
-      {/* 
-      <Contact/> */}
-     
-      </div>
-    
-  )
+    <div className="container">
+      <Main />
+      <About />
+      <Experience />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
