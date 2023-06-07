6e45bda97a82dcacc6b81bbc9acbc0db2bc31864
aosman <pianogreen0@gmail.com>
2023-06-07 14:36:24 +0100

    components

--- port/src/components/Experience.jsx
+++ port/src/components/Experience.jsx
@@ -76,10 +76,11 @@ export default function Experience() {
           </div>
           <div className={classes.title}>
 
-          <h4>title</h4>
+          <h4>Blog Central</h4>
           </div>
           <div className={classes.info}>
-            buttons for github repo and live project 
+            <button className={classes.btn}>Github Repo</button>
+            <button className={classes.btn}>Open Project</button>
           </div>
           <div className={classes.description}>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil nam neque quaerat quibusdam aliquam corporis velit, ad quia amet deserunt quae voluptate natus fugiat eum nisi aspernatur placeat laudantium. Sapiente.</p>
@@ -89,8 +90,14 @@ export default function Experience() {
           <h4>Tech used</h4>
 
           </div>
-          <div className={classes.icon}>
-            icons go here
+          <div className={classes.skillsContainer}>
+
+          <div className={classes.iconContainer}>
+            <img className={classes.icons} src="https://img.icons8.com/color/48/000000/python.png" alt="" />
+          <div>
+            Python
+          </div>
+          </div>
           </div>
 
         </div>
@@ -99,31 +106,32 @@ export default function Experience() {
       </div>
 
     </section>
-    // <div className={classes.section}>
-    //   <div className={classes.container}>
-    //     <div className={classes.left}>
-         
-    //       <ul className={classes.list}>
-    //         {data.map((item) => (
-    //           <li className={classes.item} key={item} onClick={()=> setListItem(item)}>
-    //             {item}
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     </div>
-    //     <div className={classes.right}>
-    //       {listItem === "Projects" ? (
-    //         // <h3>Hello World</h3>
-    //         <Projects />
-    //         ) : listItem === "Infrastructure & IT support" ? (
-    //         <Projects />
-    //       ) : listItem === "Skills" ? (
-    //         <Skills />
-    //       ) : (
-    //         <h3>Hello</h3>
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
+
+// <div className={classes.section}>
+//   <div className={classes.container}>
+//     <div className={classes.left}>
+     
+//       <ul className={classes.list}>
+//         {data.map((item) => (
+//           <li className={classes.item} key={item} onClick={()=> setListItem(item)}>
+//             {item}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//     <div className={classes.right}>
+//       {listItem === "Projects" ? (
+//         // <h3>Hello World</h3>
+//         <Projects />
+//         ) : listItem === "Infrastructure & IT support" ? (
+//         <Projects />
+//       ) : listItem === "Skills" ? (
+//         <Skills />
+//       ) : (
+//         <h3>Hello</h3>
+//       )}
+//     </div>
+//   </div>
+// </div>
