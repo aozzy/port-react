58989ad94af2f73697b023c8a2bd2cf78ed8c1a2
aosman <pianogreen0@gmail.com>
2023-06-12 13:52:34 +0100

    components

--- port/src/components/TimeLine.jsx
+++ port/src/components/TimeLine.jsx
@@ -4,12 +4,14 @@ export default function TimeLine() {
   return (
     <React.Fragment>
       <div className={classes.timeline}>
-        <div className={classes.container}></div>
+        <div className={classes.container}>
+
         <div className={classes.text}>
           <h4>Company Name</h4>
           <small>2021 - 2022</small>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque tempore sed, quibusdam temporibus itaque amet, doloribus culpa minus labore voluptatem voluptatum facilis vero sequi eum quos libero ducimus cumque. Ut!</p>
 
+        </div>
         </div>
       </div>
     </React.Fragment>
