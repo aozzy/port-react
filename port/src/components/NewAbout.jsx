a9e98111ab4f6c7bfe52ccfd5feb3001ba21f37b
aosman <pianogreen0@gmail.com>
2023-06-22 17:31:07 +0100

    components

---
+++ port/src/components/NewAbout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './modules/NewAbout.css'
+
+function NewAbout() {
+  return (
+    <div className=''>
+   <div className={classes.left}>
+    <div className={classes.right}>
+
+    </div>
+
+   </div>
+    </div>
+  )
+}
+
+export default NewAbout
