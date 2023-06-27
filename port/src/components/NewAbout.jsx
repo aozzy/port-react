340b3fd91cbaedbb7dc8c0bb05720e3bd8f4edb0
aosman <pianogreen0@gmail.com>
2023-06-27 16:45:25 +0100

    components

--- port/src/components/NewAbout.jsx
+++ port/src/components/NewAbout.jsx
@@ -5,7 +5,13 @@ function NewAbout() {
   return (
     <section className={classes['section-about']}>
    <div className={classes.container}>
-    
+    <div className={`${classes['grid-item']} ${classes['grid-item-1']}`}>hello</div>
+    <div className={`${classes['grid-item']} ${classes['grid-item-2']}`}></div>
+    <div className={`${classes['grid-item']} ${classes['grid-item-3']}`}></div>
+    <div className={`${classes['grid-item']} ${classes['grid-item-4']}`}></div>
+    <div className={`${classes['grid-item']} ${classes['grid-item-5']}`}></div>
+    <div className={`${classes['grid-item']} ${classes['grid-item-6']}`}></div>
+   
    </div>
     </section>
   )
