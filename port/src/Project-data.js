fe5383a934a906565f1593cd58565e23b39dc356
aosman <pianogreen0@gmail.com>
2023-07-06 13:51:26 +0100

    components

--- port/src/Project-data.js
+++ port/src/Project-data.js
@@ -1,5 +1,7 @@
 import Lottie from "lottie-react";
 import pyhton from '/python.json'
+import js from '/javascript.json'
+import react from '/react.json'
 export  const data = [
   {
     id:1,
@@ -11,11 +13,23 @@ export  const data = [
     ga:true,
     image:"./images/snake.png",
     openingDesc:"Project 1 for the General Assembly Software Immersive course. This was my first project during my time at General Assembly, It was a solo project and the duration was a week, the technologies I used are HTML CSS and JavaScript.",
-    icons:[]
+    icons:[<img width="50" height="50" src="https://img.icons8.com/ios/50/html-filetype--v1.png" alt="html-filetype--v1"/>,<img width="50" height="50" src="https://img.icons8.com/ios-filled/50/css3.png" alt="css3"/>,<Lottie animationData={js} loop={true}/>]
   
   },
   {
     id: 2,
+    title:"Project 2",
+    subtitle:"The Breaking Bad Project",
+    url:"https://aozzy.github.io/project-2/",
+    type:"Pair Programming Hackathon",
+    ga:true,
+    image:"./images/breakingbad.png",
+    openingDesc:"Project 2 was a pair programming project in which we had to build a React application that consumes a public API.",
+    icons:[  <Lottie  animationData={react} loop={true}/>],
+    techUsedDesc:"the technologies we used were React Node.js React Router Webpack Axios React Player Bulma Babel Google Fonts Git GitHub Lodash Sass and VScode Live Share."
+  },
+  {
+    id: 5,
     title: "Blog Central",
     url:"https://blog-central.onrender.com/",
     github:"https://github.com/aozzy/blog",
