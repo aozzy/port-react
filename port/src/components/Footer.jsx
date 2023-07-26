c8c6329bacc8638850c749cbdbcf42c92d0d7406
aosman <pianogreen0@gmail.com>
2023-07-26 13:24:37 +0100

    components

--- port/src/components/Footer.jsx
+++ port/src/components/Footer.jsx
@@ -4,16 +4,17 @@ function Footer() {
   return (
     <React.Fragment>
       <div className={classes.container}>
+       <div className={classes.inner}>
+
       <div className={classes.footerItem}>
       <img className={classes.icon} width="50" height="50" src="https://img.icons8.com/ios-filled/50/github.png" alt="github"/>
-       <span>https://github.com/aozzy</span>
-
+      <a>https://github.com/aozzy</a>
       </div>
       <div className={classes.footerItem}>
-      <img className={classes.icon} width="50" height="50" src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin"/>
-       <span>www.linkedin.com/in/abdi-osman-000</span>
-
+      <img className={classes.icon} width="50" height="50" src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin"/>     
+      <a>www.linkedin.com/in/abdi-osman-000</a>
       </div>
+       </div>
       </div>
     </React.Fragment>
   )
