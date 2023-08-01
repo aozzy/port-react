72f7a0a8d8f3251177e09aff19cd58136e13f095
aosman <pianogreen0@gmail.com>
2023-08-01 14:14:11 +0100

    components

--- port/src/components/NewTimeline.jsx
+++ port/src/components/NewTimeline.jsx
@@ -19,6 +19,7 @@ function NewTimeline() {
     <h4 className="vertical-timeline-element-subtitle">Online</h4>
     <p>
    I completed a skills bootcamp with the Lightbulb learning new skills like unreal engine and got to mentor junior developers.  
+   <br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
         
@@ -33,7 +34,7 @@ function NewTimeline() {
     <p>
    I completed the following Udemy courses: The complete JavaScript course 20222, 100 days of Python, React the complete guide,
    The Web Developer Bootcamp 2022
-    </p>
+    </p> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
@@ -47,7 +48,8 @@ function NewTimeline() {
 IT Support Engineer, Contract</h3>
     <h4 className="vertical-timeline-element-subtitle">Medpace, London</h4>
     <p>
-      Supporting a base of 400 users in the London office, Providing 1st & 2nd Line Support 
+      Supporting a base of 400 users in the London office, Providing 1st & 2nd Line Support.
+      <br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
    <VerticalTimelineElement
@@ -60,7 +62,8 @@ IT Support Engineer, Contract</h3>
     <h3 className="vertical-timeline-element-title">Software Engineering Immersive student</h3>
     <h4 className="vertical-timeline-element-subtitle">General Assembly, London</h4>
     <p>
-    During the 12 week Software Engineering Immersive course I learned about the fundamentals of web development, the Creation of front-end web applications using Javascript, React, HTML5, CSS & SCSS and how to develop and deploy full-stack applications with technologies including Python with Flask and Express with Node.js. I also learned how to consume and integrate third-party APIs and got to practice version control with Git and Github.
+    During the 12 week Software Engineering Immersive course I learned about the fundamentals of web development, the Creation of full-stack applications.
+   <br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   
@@ -73,7 +76,8 @@ IT Support Engineer, Contract</h3>
     <h3 className="vertical-timeline-element-title">Field and Ad hoc Engineer, Contract</h3>
     <h4 className="vertical-timeline-element-subtitle">Wavex, London</h4>
     <p>
-    Providing desketop support & Ad-Hoc infrastructure support and bespoke infrastructure checks for Wavex clients at Wavex HQ and at the client’s site, with a user base of over 2000 users across the professional services industry
+    Providing desketop support & Ad-Hoc infrastructure support and bespoke infrastructure checks for Wavex clients at Wavex HQ and at the client’s site, with a user base of over 2000 users across the professional services industry.
+    <br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -86,8 +90,8 @@ IT Support Engineer, Contract</h3>
     <h4 className="vertical-timeline-element-subtitle">Wavex, London</h4>
     <p>
 Working within an ISO27001 environment proving high quality IT & Infrastructure support to Wavex clients.
-Ability to work effectively with minimal supervision and the ability to prioritise tasks appropriate
-Providing Desktop Support and bespoke infrastructure checks for Wavex clients at Wavex HQ and at the client’s site, with a user base of over 2000 users across the professional services industry.
+Ability to work effectively with minimal supervision and the ability to prioritise tasks.
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -101,6 +105,7 @@ Providing Desktop Support and bespoke infrastructure checks for Wavex clients at
     <p>
     Providing 3rd & 2nd Line Support to a user base of 250 users, Working in a team of two.
 Administration and maintenance of all IT infrastructure, Supporting Windows Server 2003 R2. 
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -113,8 +118,9 @@ Administration and maintenance of all IT infrastructure, Supporting Windows Serv
     <h4 className="vertical-timeline-element-subtitle">RIBA, London</h4>
     <p>
 Providing 2nd Line support to a user base of 400 users, Desk side phone and remote support.
-Taking ownership of tickets escalated from 1st line support, also participating in 3rd line projects
-Supporting Windows Server 2008r2 Server 2012r2 and 2016 datacentre, Windows 7 and OSX operating systems.
+Taking ownership of tickets escalated from 1st line support, also participating in 3rd line projects.
+
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -129,6 +135,7 @@ Supporting Windows Server 2008r2 Server 2012r2 and 2016 datacentre, Windows 7 an
 Working in a team of four rolling out 0365 to a user base of 1600
 Installing Office 2016 on windows 7/10 and OSX devices.
 Ensuring all previous versions of office has been uninstalled off every device.
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -142,7 +149,8 @@ Ensuring all previous versions of office has been uninstalled off every device.
     <p>
 Providing Technical support to a user base of over 3500 staff and Research students.
 Ensuring deskside, phone and AV support across all LSBU Southbank sites.
-Logging Tickets using Topdesk ITSM, Resolving Tickets or escalating tickets to the relevant teams.
+
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
     </p>
   </VerticalTimelineElement>
   
@@ -157,8 +165,8 @@ Logging Tickets using Topdesk ITSM, Resolving Tickets or escalating tickets to t
       <p>
 Imaging and building 2000+ laptops and desktop machines as part of a rollout project.
 Configuring Lenovo Yoga’s 260 and Dell Latitude E5270 Laptops for staff in the UK France Germany Spain the Netherlands Belgium Switzerland Italy Portugal Romania and the US.
-Deploying images using SCCM and using Counter Act to whitelist the machines.
-Ensuring all users has access to their VDI once migrated to Windows 10.
+
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
       </p>
     </VerticalTimelineElement>
   
@@ -175,6 +183,7 @@ Ensuring all users has access to their VDI once migrated to Windows 10.
       <p>
       •Working within a team of 8 supporting a user base of 4000 users.
 Providing 1st / 2nd line resolutions where applicable and logging calls via the Service Now system and assigning tickets to the relevant teams.
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
       </p>
     </VerticalTimelineElement>
     <VerticalTimelineElement
@@ -189,6 +198,7 @@ Providing 1st / 2nd line resolutions where applicable and logging calls via the 
 Working in a Team of 3 providing deskside and phone support to over 400 users.
 Creating network accounts for new joiners using Active Directory and MS Exchange.
 Logging and Resolving Tickets through the Altiris ticking system and the Cherwell system. 
+<br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
 
       </p>
     </VerticalTimelineElement>
@@ -202,9 +212,10 @@ Logging and Resolving Tickets through the Altiris ticking system and the Cherwel
       <h4 className="vertical-timeline-element-subtitle">GroupM, London</h4>
       <p>
   Working alongside IBM staff supporting 2000+ users across four Group M sites in London.
- Troubleshooting printer issues, replacing toner and fusers, fixing paper jams and solving printer errors.
 Imaging and configuring laptops and desktops using LANDesk. 
  Configuring Macs and installing requested software for users. Re-imaging and software installation of rebuilds.
+ 
+ <br/> <a className={classes.link} href="https://www.linkedin.com/in/abdi-osman-000/details/experience/" target='_blank'>Read more here</a> 
 
       </p>
     </VerticalTimelineElement>
